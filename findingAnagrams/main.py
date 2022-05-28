718c70d6a5ea371af774f1c5a6ff23e0f9e1ee93
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-28 18:03:48 +0100

    finished anagram

--- findingAnagrams/main.py
+++ findingAnagrams/main.py
@@ -4,25 +4,10 @@
 # find_anagrams("below", "elbow") --> True
 
 
-### def find_anagram(word, anagram):
-#    if (sorted(word) == sorted(anagram)):
- #       return True
- #   else:
-  #      return False
-#
-#word = 
-#anagram = )
-#find_anagram(word, anagram) 
+def find_anagram(word, anagram):
+    return sorted(word) == sorted(anagram)
 
+word = str(input("Enter a word: "))
+anagram = str(input("Enter an anagram: "))
 
-def check(s1, s2):
-    # the sorted strings are checked
-    if(sorted(s1)== sorted(s2)):
-        return True
-    else:
-        return False    
-         
-# driver code 
-s1 = str(input('Enter the word: '))
-s2 = str(input('Enter the anagram: '))
-check(s1, s2)
+print(find_anagram(word, anagram))
--- readingTextFiles/main.py
+++ readingTextFiles/main.py
@@ -4,13 +4,14 @@
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
-
+    with open(filename) as story:
+        return story.read()
 
 def count_words():
     text = read_file_content("./story.txt")
-    # [assignment] Add your code here
+    words = text.split()
 
-    return {"as": 10, "would": 20}
+    return words
+
+count = count_words()
+print(count)
