b9cc3f705144cdc4b52f13be9d37ca0f697f04a3
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-28 15:26:07 +0100

    added other unzipped projects

--- findingAnagrams/main.py
+++ findingAnagrams/main.py
@@ -4,8 +4,25 @@
 # find_anagrams("below", "elbow") --> True
 
 
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
+### def find_anagram(word, anagram):
+#    if (sorted(word) == sorted(anagram)):
+ #       return True
+ #   else:
+  #      return False
+#
+#word = 
+#anagram = )
+#find_anagram(word, anagram) 
 
-    return True
 
+def check(s1, s2):
+    # the sorted strings are checked
+    if(sorted(s1)== sorted(s2)):
+        return True
+    else:
+        return False    
+         
+# driver code 
+s1 = str(input('Enter the word: '))
+s2 = str(input('Enter the anagram: '))
+check(s1, s2)
