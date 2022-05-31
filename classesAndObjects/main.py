e27755814b66c0b1c4b7204f1fe151970e61a033
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-31 03:34:34 +0100

    initialised some variables in Student class

--- classesAndObjects/main.py
+++ classesAndObjects/main.py
@@ -1,10 +1,19 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
-
-
-
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score      
+    
+    def change_name(self, name):
+        print(self.tracks)
+    def change_age(self):
+        print(self.tracks)
+    def add_track(self):
+        print(self.tracks)
+    def get_score(self):
+        print(self.tracks)
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
